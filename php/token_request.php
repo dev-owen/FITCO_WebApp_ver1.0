ebf9663d0e02e49378032d6903a7eb4c713cbabc
wonjongOh94 <jgyuity1289@gmail.com>
2019-02-28 16:00:12 +0900

    20190228 access_token_get

--- php/token_request.php
+++ php/token_request.php
@@ -1,12 +1,18 @@
 <?
-$username = 'Tfrj19ZXMKoPPW3ndxigAD';
-$password = 'RcGVBeXWHC1TpKwfy6koWN';
 
-$url = 'https://brummieowen.cafe24api.com/api/v2/oauth/token';
+$code = substr($_GET['code'], 0, 22);
+$mall_id = $_GET['mall_id'];
+echo $code;
+echo $mall_id;
+
+$username = 'IuFeyYTufEhbiXGby9XESH';
+$password = 'AHAKnaW1JBdgocqZVnM0HB';
+
+$url = 'https://'+$mall_id+'.cafe24api.com/api/v2/oauth/token';
 
 $param = array(
     'grant_type'   => 'authorization_code'
-    ,'code'   => 'qNf1ujemZkCzu2d0bmlKYL'
+    ,'code'   => $code
     ,'redirect_uri' => 'https://jgyuity.cafe24.com/fitco/php/token_response.php'
 );
 
@@ -23,6 +29,7 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 $result = curl_exec($ch);
 $result = json_decode($result,true);
 print_r($result);
+echo $result;
 curl_close($ch);
 
 ?>
